d7596f9ef6159847ed923f596540ef49e16ee440
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-02-26 00:05:52 -0300

    MenuData`s dishes attribute - fixed

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -12,21 +12,22 @@ class MenuData:
         with open(source_path, "r") as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
-                dish_name = row["dish"]
-                dish_price = float(row["price"])
-                ingredient_name = row["ingredient"]
-                recipe_amount = int(row["recipe_amount"])
+                name = row["dish"]
+                price = float(row["price"])
+                ingredient = Ingredient(row["ingredient"])
+                amount = int(row["recipe_amount"])
 
-                dish = Dish(dish_name, dish_price)
-
-                ingredient = Ingredient(ingredient_name)
-                dish.add_ingredient_dependency(ingredient, recipe_amount)
-
+                dish = Dish(name, price)
                 if dish not in self.dishes:
+                    dish.add_ingredient_dependency(ingredient, amount)
                     self.dishes.add(dish)
+                else:
+                    for dish in self.dishes:
+                        if dish.name == name:
+                            dish.add_ingredient_dependency(ingredient, amount)
 
 
 if __name__ == "__main__":
     menu = MenuData("data/menu_base_data.csv")
     for dish in menu.dishes:
-        print(repr(dish))
+        print(dish.get_ingredients())
